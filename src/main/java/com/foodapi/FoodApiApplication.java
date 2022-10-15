dcd3a3454d3d586be71ddc8d91604dd895d8d011
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2022-10-15 15:45:27 -0300

    Criando projeto e instalando as dependencias

---
+++ src/main/java/com/foodapi/FoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.foodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodApiApplication.class, args);
+	}
+
+}
