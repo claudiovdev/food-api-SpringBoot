402f38e24c504ac29f71819450cc635eaf747571
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2022-10-17 20:59:53 -0300

    Definindo uma busca por Id e implementando o @PathVariable

--- src/main/java/com/food/api/controllers/CozinhaController.java
+++ src/main/java/com/food/api/controllers/CozinhaController.java
@@ -3,7 +3,9 @@ package com.food.api.controllers;
 import com.food.domain.model.Cozinha;
 import com.food.domain.repositoryes.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +18,14 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)   //Com essa produces definimos que o retnoro será sempre em Json á nivel de método
     public List<Cozinha> listar(){
         return cozinhaRepository.listar();
+    }
+
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable("cozinhaId") Long id){
+        return  cozinhaRepository.buscar(id);
     }
 
 }
