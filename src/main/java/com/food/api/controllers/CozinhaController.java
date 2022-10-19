97a60e82656aa432c005eb2ed9f33e6cc62e61c0
claudiovdev <c.viniciusmagalhaes@gmail.com>
2022-10-19 09:25:41 -0300

    Adicionando metedo post para criar uma cozinha

--- src/main/java/com/food/api/controllers/CozinhaController.java
+++ src/main/java/com/food/api/controllers/CozinhaController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,6 +33,12 @@ public class CozinhaController {
         }
 
        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha salvar(@RequestBody Cozinha cozinha){
+        return cozinhaRepository.salvar(cozinha);
     }
 
 }
