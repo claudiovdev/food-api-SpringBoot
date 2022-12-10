f739ad80198714fbaa87e4f4f91cca10b2a7eb13
claudiovdev <c.viniciusmagalhaes@gmail.com>
2022-12-10 14:07:29 -0300

    Adicionando novo metodo para utilizar o padrão de data UTC

--- src/main/java/com/food/FoodApplication.java
+++ src/main/java/com/food/FoodApplication.java
@@ -3,10 +3,15 @@ package com.food;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class FoodApplication {
 
 	public static void main(String[] args) {
+
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC")); // Este método está sendo implementado para pordemros usar como padrão o TimeZone UTC
+
 		SpringApplication.run(FoodApplication.class, args);
 	}
 
