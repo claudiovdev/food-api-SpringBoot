17f68a311a342b120fdac1d24071d3a031b47f9f
claudiovdev <c.viniciusmagalhaes@gmail.com>
2022-12-21 18:26:41 -0300

    Adicionando cidaderesumomodel

---
+++ src/main/java/com/food/api/model/models/CidadeResumoModel.java
@@ -0,0 +1,13 @@
+package com.food.api.model.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CidadeResumoModel {
+
+    private Long id;
+    private String nome;
+    private String nomeEstado;
+}
---
+++ src/main/java/com/food/api/model/models/EnderecoModel.java
@@ -0,0 +1,18 @@
+package com.food.api.model.models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EnderecoModel {
+
+
+    private String cep;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private CidadeResumoModel cidade;
+}
