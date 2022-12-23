3b0d740769e9e0d1034d8b9ffe7a1c5c92ad4878
claudiovdev <c.viniciusmagalhaes@gmail.com>
2022-12-23 08:04:51 -0300

    Adicionando nova regra para email já cadastrado e tratando o transacional para não influencia no cadastro ou alteração dos usuario

--- src/main/java/com/food/domain/repositoryes/UsuarioRepository.java
+++ src/main/java/com/food/domain/repositoryes/UsuarioRepository.java
@@ -4,6 +4,10 @@ import com.food.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends CustomJpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
 }
